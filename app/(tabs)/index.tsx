9e77e6fa5074138ac39f5d9a2d6888a858e5ca9f
huy <huylee2044@gmail.com>
2025-10-04 22:58:08 +0700

    firstcommit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet,View,Text,Pressable,ScrollView } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
@@ -9,90 +9,96 @@ import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView>
+     
+    <View style={{height:800}}>
+      <View style={styles.head} >
+        <Text style={{fontSize:26,textAlign:"center",flexDirection:"column",fontWeight:400,marginBottom:30}}> A premium online store for sporter and their stylihsh choice</Text>
+      </View>
+       <View style={styles.image} >
+           <View style={{flex:4,flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+               <View style={{height:359,width:388,borderRadius:50,justifyContent:"center",alignItems:"center",backgroundColor:"#E941411A"}}>
+                  <Image source={require('../../assets/images/bifour_-removebg-preview.png')}   style={{ width: 200, height: 200, resizeMode: "contain" }}  ></Image>
+               </View>
+           </View>
+           <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+              <Text style={{fontSize:26,textAlign:"center",flexDirection:"column",fontWeight:700,width:160}}>POWER BIKE SHOP</Text>
+           </View>
+       </View>
+       <View style={styles.buttonarea} >
+          <Pressable style={{width:340,height:61,backgroundColor:"#E94141",alignItems:"center",justifyContent:"center",borderRadius:30,}}  onPress={() => {
+           
+          }}>
+          <Text style={{fontSize:24,color:"white"}}> Get Started</Text>
+          </Pressable>
+       </View>
+       
+     
+    </View>
+    
+  
+  
+    </ScrollView>
+    // <ParallaxScrollView
+    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    //   headerImage={
+    //     <Image
+    //       source={require('@/assets/images/partial-react-logo.png')}
+          
+    //     />
+    //   }>
+     
+    // <View style={{height:800}}>
+    //   <View style={styles.head} >
+    //     <Text style={{fontSize:26,textAlign:"center",flexDirection:"column",fontWeight:400,marginBottom:30}}> A premium online store for sporter and their stylihsh choice</Text>
+    //   </View>
+    //    <View style={styles.image} >
+    //        <View style={{flex:4,flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+    //            <View style={{height:359,width:388,borderRadius:50,justifyContent:"center",alignItems:"center",backgroundColor:"#E941411A"}}>
+    //               <Image source={require("@/assets/images")} ></Image>
+    //            </View>
+    //        </View>
+    //        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+    //           <Text style={{fontSize:26,textAlign:"center",flexDirection:"column",fontWeight:700,width:160}}>POWER BIKE SHOP</Text>
+    //        </View>
+    //    </View>
+    //    <View style={styles.buttonarea} >
+    //       <Pressable style={{width:340,height:61,backgroundColor:"#E94141",alignItems:"center",justifyContent:"center",borderRadius:30,}}  onPress={() => {
+           
+    //       }}>
+    //       <Text style={{fontSize:24,color:"white"}}> Get Started</Text>
+    //       </Pressable>
+    //    </View>
+       
+     
+    // </View>
+    
+  
+  
+    // </ParallaxScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const styles= StyleSheet.create(
+  {
+    container:{
+      backgroundColor:"black",
+    },
+    head:{
+       backgroundColor:"white",
+       justifyContent:"flex-end",
+       flex:1
+    },
+    image:{
+      flex:3,
+      backgroundColor:"white",
+      flexDirection:"column"
+    },
+     buttonarea:{
+       backgroundColor:"white",
+       justifyContent:"center",
+       alignItems:"center",
+       flex:1
+    }
+  }
+)
